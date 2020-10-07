83d7f8e8b198093d6f77d3704175a19555547a67
segal microbiome lab <35376355+segalmicrobiomelab@users.noreply.github.com>
2020-10-07 13:04:43 -0400

    Update QIIME2_phyloseq_upload.R

--- QIIME2_phyloseq_upload.R
+++ QIIME2_phyloseq_upload.R
@@ -30,43 +30,12 @@ BiocManager::install("phyloseq")
 
 update.packages()
 
-phy<-qza_to_phyloseq(features="filtered.merge.table.two.qza", tree="rooted-tree.qza", taxonomy="taxonomy.two.qza",metadata="MSQ.mouse.master.map.two.txt")
-
-random_tree = rtree(ntaxa(phy), rooted=TRUE, tip.label=taxa_names(phy))
-plot(random_tree)
-
-#Give a colnames to separate different taxonomic levels
-colnames(tax_table(phy))=c("Domain", "Phylum", "Class", "Order", "Family", "Genus", "OTU")
-
-rownames(sample_data(phy))
-colnames(sample_data(phy))
-
-# Remove taxa with 0 abundance
-phy = subset_taxa(phy, rowSums(otu_table(phy)) != 0)
-
-##If you want to nomalize OTU table before
-## To normalize data you need to set a function
-normalizeSample = function(x) {
-    x/sum(x)
-}
-phy.relative.table = transformSampleCounts(phy, normalizeSample)
-
-rownames(sample_data(phy.relative.table))
-colnames(sample_data(phy.relative.table))
-
-Phylum.rel.table = tax_glom(phy.relative.table, taxrank = "Phylum")
-Class.rel.table = tax_glom(phy.relative.table, taxrank = "Class")
-Order.rel.table = tax_glom(phy.relative.table, taxrank = "Order")
-Family.rel.table = tax_glom(phy.relative.table, taxrank = "Family")
-Genus.rel.table = tax_glom(phy.relative.table, taxrank = "Genus")
-OTU.rel.table = tax_glom(phy.relative.table, taxrank = "OTU")
-
 #################################################################
 #################################################################
 #################################################################
 
-save.image(file="191017.short.RData")
-load(file="191017.short.RData")
+save.image(file="mouse.RData")
+load(file="mouse.RData")
 
 #################################################################
 #################################################################
